520d8e9097aaaef70ff96e87fa2483d0c4f1cbe0
渡辺友美 <tncnkncn.0329@gmail.com>
2023-03-15 19:09:33 +0900

    ユーザー名からそのユーザー名の詳細ページへ推移する修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
+#class UsersController < ApplicationController
+ # def show
+  #  @user= current_user
+   # @prototypes = user.prototypes
+  #end
+#end
 class UsersController < ApplicationController
   def show
-    @user= current_user
-    @prototypes = current_user.prototypes
+    @user = User.find(params[:id])
+    @prototypes = @user.prototypes
   end
 end
-
